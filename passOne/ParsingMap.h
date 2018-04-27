dfd90b3d6f2919e521af3ecb6893cf244da3487d
Magho <mmaghraby134@gmail.com>
2018-04-27 18:11:42 +0200

    Create ParsingMap.h

---
+++ passOne/ParsingMap.h
@@ -0,0 +1,23 @@
+//
+// Created by Magho on 27-Apr-18.
+//
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+class ParsingMap {
+
+    public:
+
+        list<map<string, string>> parisngFunction(string );
+
+        size_t split(const std::string &txt, std::vector<std::string> &strs, char ch);
+        bool startsWith (string line, string charsStartWith);
+
+        void printTheList (list<map<string, string>> aaaaa);
+};
