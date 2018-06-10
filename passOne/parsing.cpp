4e7eca82c867ceb220239925dd9f6278c4c98970
ahm7 <elpopahmed462@gmail.com>
2018-06-11 02:22:29 +0300

    make all words small except literals and strings

--- passOne/parsing.cpp
+++ passOne/parsing.cpp
@@ -1,171 +1,171 @@
-//
-// Created by HP on 28-Apr-18.
-//
+        //
+        // Created by HP on 28-Apr-18.
+        //
 
-#include "parsing.h"
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <list>
-#include <vector>
-#include <algorithm>
-#include "Line.h"
+        #include "parsing.h"
+        #include <fstream>
+        #include <iostream>
+        #include <map>
+        #include <list>
+        #include <vector>
+        #include <algorithm>
+        #include "Line.h"
 
-list<Line> parsing :: parisngFunction (string path) {
+        list<Line> parsing :: parisngFunction (string path) {
 
-    ifstream file;
-    // path --> "/home/magho/workspaceC++/Assembler/passOne/test.txt"
-    file.open(path, ios::in);
+            ifstream file;
+            // path --> "/home/magho/workspaceC++/Assembler/passOne/test.txt"
+            file.open(path, ios::in);
 
-    list<Line> aaaaa;
-    vector<string> string1;
-    std::string str;
+            list<Line> aaaaa;
+            vector<string> string1;
+            std::string str;
 
-    try {
+            try {
 
-        while (std::getline(file, str)) {
+                while (std::getline(file, str)) {
 
-            Line bbbb;
+                    Line bbbb;
 
-            //transfer to string to lower case
-          std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-            cout << str << "\n";
-            //check if comment line
-            if (startsWith(str, ".")) {
-                bbbb.isComment = true;
-                bbbb.setcomment(str);
+                    //transfer to string to lower case
+                 // std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+                    cout << str << "\n";
+                    //check if comment line
+                    if (startsWith(str, ".")) {
+                        bbbb.isComment = true;
+                        bbbb.setcomment(str);
 
-            } else if (str.size() == 0) {
-                bbbb.isEmptyLine = true;
-            } else {
+                    } else if (str.size() == 0) {
+                        bbbb.isEmptyLine = true;
+                    } else {
 
-                split(str, string1, ' ');
+                        split(str, string1, ' ');
 
-               /* for (int i = 0 ; i < string1.size() ; i++) {
-                    cout << string1[i] << "         ";
-                }
-*/
-                for (int i = 0; i < string1.size(); i++) {
-                    if (startsWith(string1[i], ".")) {
-                        string str123 = "";
-                        for (int j = i; j < string1.size(); j++) {
-                            str123 = str123 + string1[j] + " ";
+                       /* for (int i = 0 ; i < string1.size() ; i++) {
+                            cout << string1[i] << "         ";
                         }
-                        //remove the extra space at the end
-                        str123 = str123.substr(0, str123.size() - 1);
-                        bbbb.setcomment(str123);
+        */
+                        for (int i = 0; i < string1.size(); i++) {
+                            if (startsWith(string1[i], ".")) {
+                                string str123 = "";
+                                for (int j = i; j < string1.size(); j++) {
+                                    str123 = str123 + string1[j] + " ";
+                                }
+                                //remove the extra space at the end
+                                str123 = str123.substr(0, str123.size() - 1);
+                                bbbb.setcomment(str123);
 
-                        break;
+                                break;
+                            }
+                            else if (i == 0) {
+                                char x = string1[i][0];
+                                if ((int(x) >= 65 && int(x) <= 90 )| (int(x) <= 122 && int(x) >= 97) || x=='+') {
+                                    bbbb.setWord1(string1[i]);
+                                    bbbb.NumofwORD = 1;
+                                } else {
+                                    bbbb.islabelStartWithNoChar = true;
+                                    break;
+                                }
+                            }
+                            else if (i == 1){
+
+                                bbbb.setWord2(string1[i]);
+                                bbbb.NumofwORD = 2;
+                            }
+                            else if (i == 2){
+                                bbbb.setWord3(string1[i]);
+                                bbbb.NumofwORD = 3;
+                            }
+                            else {
+                                string str123 = "";
+                                for (int j = i; j < string1.size(); j++) {
+                                    str123 = str123 + string1[j] + " ";
+                                }
+                                //remove the extra space at the end
+                                str123 = str123.substr(0,str123.size()-1);
+                                bbbb.setcomment(str123);
+
+                                break;
+                            }
+                        }
                     }
-                    else if (i == 0) {
-                        char x = string1[i][0];
-                        if ((int(x) >= 65 && int(x) <= 90 )| (int(x) <= 122 && int(x) >= 97) || x=='+') {
-                            bbbb.setWord1(string1[i]);
-                            bbbb.NumofwORD = 1;
-                        } else {
-                            bbbb.islabelStartWithNoChar = true;
+                    //add row to the list
+                    aaaaa.push_front(bbbb);
+                }
+
+            } catch (const std::invalid_argument &e) {
+                // do stuff with exception...
+                cout << "Cannot open input file.\n";
+            }
+            file.close();
+            return aaaaa;
+        }
+
+        size_t parsing :: split(const std::string &txt, std::vector<std::string> &strs, char ch) {
+
+            strs.clear();
+
+            for (int i = 0 ; i < txt.size() ; i++) {
+                if (txt[i] != ' ' && txt[i] != '\t') {
+                    if (txt.find("byte") != std::string::npos) {
+                        if ((txt[i] == 'c'||txt[i] == 'C' ) && txt[i+1] == '\'') {
+                            int k  = i;
+                            i = i+2;
+                            while (txt[i] != '\'') {
+                                i++;
+                            }
+                            strs.push_back(txt.substr(k, i));
+                            break;
+                        }
+                        if ((txt[i+1] == 'c'||txt[i+1] == 'C' ) && txt[i+2] == '\'' && txt[i] == '.') {
+                            int k  = i;
+                            i = i+3;
+                            while (txt[i] != '\'') {
+                                i++;
+                            }
+                            strs.push_back(txt.substr(k, i));
                             break;
                         }
                     }
-                    else if (i == 1){
-
-                        bbbb.setWord2(string1[i]);
-                        bbbb.NumofwORD = 2;
-                    }
-                    else if (i == 2){
-                        bbbb.setWord3(string1[i]);
-                        bbbb.NumofwORD = 3;
-                    }
-                    else {
-                        string str123 = "";
-                        for (int j = i; j < string1.size(); j++) {
-                            str123 = str123 + string1[j] + " ";
+                    for (int j = i ; j < txt.size() ; j++ ) {
+                        if (txt[j] == ' ' || txt[j] == '\t' ||( j == txt.size() -1) ) {
+                            if (j == txt.size() -1) {
+                                strs.push_back(txt.substr(i, j-i+1));
+                            }
+                            else {
+                                strs.push_back(txt.substr(i, j - i));
+                            }
+                            i = j;
+                            break;
                         }
-                        //remove the extra space at the end
-                        str123 = str123.substr(0,str123.size()-1);
-                        bbbb.setcomment(str123);
-
-                        break;
                     }
                 }
             }
-            //add row to the list
-            aaaaa.push_front(bbbb);
+
+            return strs.size();
         }
 
-    } catch (const std::invalid_argument &e) {
-        // do stuff with exception...
-        cout << "Cannot open input file.\n";
-    }
-    file.close();
-    return aaaaa;
-}
-
-size_t parsing :: split(const std::string &txt, std::vector<std::string> &strs, char ch) {
-
-    strs.clear();
-
-    for (int i = 0 ; i < txt.size() ; i++) {
-        if (txt[i] != ' ' && txt[i] != '\t') {
-            if (txt.find("byte") != std::string::npos) {
-                if (txt[i] == 'c' && txt[i+1] == '\'') {
-                    int k  = i;
-                    i = i+2;
-                    while (txt[i] != '\'') {
-                        i++;
-                    }
-                    strs.push_back(txt.substr(k, i));
-                    break;
-                }
-                if (txt[i+1] == 'c' && txt[i+2] == '\'' && txt[i] == '.') {
-                    int k  = i;
-                    i = i+3;
-                    while (txt[i] != '\'') {
-                        i++;
-                    }
-                    strs.push_back(txt.substr(k, i));
-                    break;
-                }
-            }
-            for (int j = i ; j < txt.size() ; j++ ) {
-                if (txt[j] == ' ' || txt[j] == '\t' ||( j == txt.size() -1) ) {
-                    if (j == txt.size() -1) {
-                        strs.push_back(txt.substr(i, j-i+1));
-                    }
-                    else {
-                        strs.push_back(txt.substr(i, j - i));
-                    }
-                    i = j;
-                    break;
-                }
-            }
+        bool parsing :: startsWith (string line, string charsStartWith) {
+            if(line.length() < charsStartWith.length())
+                return false;
+            return (line.compare(0, charsStartWith.length(), charsStartWith) == 0);
         }
-    }
 
-    return strs.size();
-}
+        void parsing :: printTheList (list<Line> aaaaa) {
+            while ( aaaaa.size() != 0) {
+                Line bbbb;
+                bbbb = aaaaa.front();
+                aaaaa.pop_front();
 
-bool parsing :: startsWith (string line, string charsStartWith) {
-    if(line.length() < charsStartWith.length())
-        return false;
-    return (line.compare(0, charsStartWith.length(), charsStartWith) == 0);
-}
+                cout << "comment is " + bbbb.getcomment()<<" ";
+                cout << "word1" + bbbb.getWord1()<<" ";
+                cout << "word2" + bbbb.getWord2()<<" ";
+                cout << "word3" + bbbb.getWord3()<<" ";
 
-void parsing :: printTheList (list<Line> aaaaa) {
-    while ( aaaaa.size() != 0) {
-        Line bbbb;
-        bbbb = aaaaa.front();
-        aaaaa.pop_front();
+                printf("is comment: %d  ", bbbb.isComment);
+                printf("number of words: %d  ",bbbb.NumofwORD);
+                printf("is More than: %d\n",bbbb.isMoreThanFourWords);
+            }
 
-        cout << "comment is " + bbbb.getcomment()<<" ";
-        cout << "word1" + bbbb.getWord1()<<" ";
-        cout << "word2" + bbbb.getWord2()<<" ";
-        cout << "word3" + bbbb.getWord3()<<" ";
-
-        printf("is comment: %d  ", bbbb.isComment);
-        printf("number of words: %d  ",bbbb.NumofwORD);
-        printf("is More than: %d\n",bbbb.isMoreThanFourWords);
-    }
-
-}
+        }
 
--- passOne/validation.cpp
+++ passOne/validation.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <algorithm>
 #include "validation.h"
 #include "Row.h"
 #include "optable.h"
@@ -22,12 +23,46 @@ void validation ::validate() {
 
     int size = TheParsinglist.size();
 
+
     for (int i = 0; i < size; i++) {
 
         Row row;
         optable opT;
         opT.setTable();
         Line line = TheParsinglist.back();
+
+        if(line.getWord1().find("=") != std::string::npos || line.getWord1().find("/'") != std::string::npos){
+
+        }else{
+            std :: string str = line.getWord1() ;
+
+            std::transform(str.begin(), str.end(),str.begin(), ::tolower);
+            line.setWord1(str);
+
+        }
+        if(line.getWord2().find("=") != std::string::npos || line.getWord2().find("/'") != std::string::npos){
+
+        }else{
+
+            std :: string str = line.getWord2() ;
+
+            std::transform(str.begin(), str.end(),str.begin(), ::tolower);
+line.setWord2(str);
+        }
+        if(line.getWord3().find("=") != std::string::npos || line.getWord3().find("/'") != std::string::npos){
+
+        }else{
+
+            std :: string str = line.getWord3() ;
+
+            std::transform(str.begin(), str.end(),str.begin(), ::tolower);
+            line.setWord3(str);
+
+        }
+
+
+
+
         TheParsinglist.pop_back();
         if (line.isMoreThanFourWords){
             row.errorMessge = " more than the availaple words";
@@ -38,6 +73,8 @@ void validation ::validate() {
             row.errorMessge = " empty line";
             row.hasError = true;
         }
+
+
         if(line.islabelStartWithNoChar){
             row.errorMessge = "labelStartWithNoChar";
             row.hasError = true;
@@ -50,12 +87,11 @@ void validation ::validate() {
 
         // cout<< line.getWord1()<<" "<<line.getWord2()<<" " <<line.getWord3()<<line.NumofwORD<<endl;
 
+
         int counter =0;
         if(line.getWord1().length()>0&&line.getWord1().at(0)=='+'){
             counter ++;
             std::string temp = line.getWord1().substr(1,line.getWord1().length());
-
-            //std::transform(str.begin(), str.end(), str.begin(), ::tolower);
             if (opT.opTable.find(temp) == opT.opTable.end()){
                 row.errorMessge="un correct format";
                 row.hasError= true;
@@ -424,9 +460,10 @@ void validation ::validate() {
 
             if(line.getWord2()=="byte"){
 
-                if(line.getWord3().length()<4 &&(line.getWord3().at(0)!='c' || line.getWord3().at(0)!='x') ||line.getWord3().at(line.getWord3().length()-1)!='\'' ||line.getWord3().at(1)!='\'' ){
+                if(line.getWord3().length()<4 &&(line.getWord3().at(0)!='c' && line.getWord3().at(0)!='x' && line.getWord3().at(0)!='C' && line.getWord3().at(0)!='X' ) ||line.getWord3().at(line.getWord3().length()-1)!='\'' ||line.getWord3().at(1)!='\'' ){
                     row.errorMessge="syntax error  not  the right format c'sds'";
                     row.hasError=true;
+
                 }
             }
 
@@ -460,7 +497,6 @@ void validation ::validate() {
     }
     // second  check if there is only one word so if it's not an op_code there is an error here
     /* else if (temp.size() == 1) {
-
          if (opT.opTable.find(temp.at("word1")) == opT.opTable.end()) {
              // not found
 //                row.setErrorMessage(" No op_code is exist");
@@ -468,96 +504,56 @@ void validation ::validate() {
              // found
              row.setop_code(temp.at("word1"));
              validationRows.push_back(row);
-
          }
-
-
-
 //third check if size is 2 so if not one of them is op_code there is error
 //so it will be on of three (label,op),(op,operand),(operand,comment) and the last one is an error
-
      } else if (temp.size() == 2) {
-
          // first(third) check is op is the firsr word
-
          if (opT.opTable.find(temp.at("word1")) != opT.opTable.end()) {
              // found
              row.setop_code(temp.at("word1"));
-
              if (temp.find("word2") != temp.end()) {
                  row.setOperand(temp.at("word2"));
              }else{   row.setcomment(temp.at("comment")); }
-
-
          }
 // second(third) check if op_code is the second word
      else if (opT.opTable.find(temp.at("word2")) != opT.opTable.end()){
-
              // found
              row.setLabel(temp.at("word1"));
              row.setop_code(temp.at("word2"));
-
-
      }
      // if not case of the above then there is error
      else {
-
 //                row.setErrorMessage("undefined op_code field");
-
          }
-
          // Fourth if size = 3 so there is 2 possible cases
          // (label,op,operand)
          } else if(temp.size() == 3){
-
          if (opT.opTable.find(temp.at("word1")) != opT.opTable.end()) {
              // found
              row.setop_code(temp.at("word1"));
              row.setOperand(temp.at("word2"));
              row.setcomment(temp.at("comment"));
-
          }
              // second(Fourth) check if op_code is the second word
          else if (opT.opTable.find(temp.at("word2")) != opT.opTable.end()){
-
              // found
              row.setLabel(temp.at("word1"));
              row.setop_code(temp.at("word2"));
              if (temp.find("word3") != temp.end()){
                  row.setOperand(temp.at("word3"));
-
              } else {row.setcomment(temp.at("comment"));}
-
          }
-
-
-
-
      } else if (temp.size() ==4){
-
          if (opT.opTable.find(temp.at("word2")) != opT.opTable.end()){
-
              row.setLabel(temp.at("word1"));
              row.setop_code(temp.at("word2"));
              row.setOperand(temp.at("word3"));
              row.setcomment(temp.at("comment"));
-
-
-
          }else{
-
 //                row.setErrorMessage("undefined op_code");
          }
-
-
-
      }
      validationRows.push_back(row);
-
-
  }
-
-*/    }
-
-
-
+*/ }
